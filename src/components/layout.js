002ea8aefaf55e881dd6a59cee5c355d6b89ea01
Steph Barden <stephyx.web@gmail.com>
2018-09-21 19:43:15 +0100

    attempting to put the js particles in... and failing for the moment

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
+import Particles from '../js-particles/particles.js'
+import ParticlesConfig from '../js-particles/particles.babel'
+import ParticlesApp from '../js-particles/particle-app.js'
+import SmallParticleScript from '../js-particles/small-script.js'
 import './main.css'
 import './style.css'
 
@@ -21,15 +25,23 @@ const Layout = ({ children }) => (
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
+            { httpEquiv: 'X-UA-Compatible', content: 'ie=edge' },
             { name: 'description', content: 'StephyX - Professional and Bespoke Web Development.' },
             { name: 'keywords', content: 'web development, stephyx, steph barden, southampton web design, southampton website, web design' },
           ]}
         >
           <html lang="en" />
+          <Particles/>
+          <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+          <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet"></link>
+          <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
         </Helmet>
         <div className='flex'>
           <div className="sidebar">
-            {/*<div id="particles-js"></div>*/}
+            <ParticlesConfig/>
+            <ParticlesApp/>
+            <SmallParticleScript/>
+            <div id="particles-js"></div>
             <img alt="Logo" id="menu" src={require('../images/menu-icon.png')}></img>
 
             <img alt="profile" id="profile_img" src="https://avatars0.githubusercontent.com/u/32325240?s=460&v=4">
