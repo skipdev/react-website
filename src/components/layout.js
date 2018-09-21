bff13a62247cee9d702dbdca89a01647e9a65454
Steph Barden <stephyx.web@gmail.com>
2018-09-21 18:51:15 +0100

    yay homepage layouts finally work!

--- src/components/layout.js
+++ src/components/layout.js
@@ -27,38 +27,31 @@ const Layout = ({ children }) => (
         >
           <html lang="en" />
         </Helmet>
-        <div className="sidebar">
-          {/*<div id="particles-js"></div>*/}
-          <img alt="Logo" id="menu" src={require('../images/menu-icon.png')}></img>
+        <div className='flex'>
+          <div className="sidebar">
+            {/*<div id="particles-js"></div>*/}
+            <img alt="Logo" id="menu" src={require('../images/menu-icon.png')}></img>
 
-          <img alt="profile" id="profile_img" src="https://avatars0.githubusercontent.com/u/32325240?s=460&v=4">
-          </img>
-          <br></br>
-          <p><b>Stephy</b></p>
-          <br></br>
-          <nav className="menu">
-            <a><img alt="x" src={require('../images/x.png')}></img></a>
+            <img alt="profile" id="profile_img" src="https://avatars0.githubusercontent.com/u/32325240?s=460&v=4">
+            </img>
             <br></br>
+            <p><b>Stephy</b></p>
             <br></br>
-          </nav>
-          <ul>
-            <li><a href="https://www.twitter.com/st3phyx_x"><i className="fab fa-twitter fa-2x"></i></a></li>
-            <li><a href="https://github.com/stephyx/"><i className="fab fa-github fa-2x"></i></a>
-            </li>
-            <li><a href="https://www.behance.net/stephyx"><i className="fab fa-behance fa-2x"></i></a>
-            </li>
-            <li><a href="https://frozenfanta.deviantart.com/"><i className="fab fa-deviantart fa-2x"></i></a></li>
-          </ul>
-        </div>
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
+            <nav className="menu">
+              <a><img alt="x" src={require('../images/x.png')}></img></a>
+              <br></br>
+              <br></br>
+            </nav>
+            <ul>
+              <li><a href="https://www.twitter.com/st3phyx_x"><i className="fab fa-twitter fa-2x"></i></a></li>
+              <li><a href="https://github.com/stephyx/"><i className="fab fa-github fa-2x"></i></a>
+              </li>
+              <li><a href="https://www.behance.net/stephyx"><i className="fab fa-behance fa-2x"></i></a>
+              </li>
+              <li><a href="https://frozenfanta.deviantart.com/"><i className="fab fa-deviantart fa-2x"></i></a></li>
+            </ul>
+          </div>
+            {children}
         </div>
       </>
     )}
