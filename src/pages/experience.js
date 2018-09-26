947ae83752f8fc1c91cf6f5d2ea7b87735164d46
Steph Barden <stephyx.web@gmail.com>
2018-09-26 14:11:47 +0100

    added the timeline

--- src/pages/experience.js
+++ src/pages/experience.js
@@ -15,42 +15,76 @@ class ExperiencePage extends Component {
           <div id="top">
             <p></p>
           </div>
-          <div id="about">
-            <p className="title">Experiences</p>
             <VerticalTimeline>
               <VerticalTimelineElement
                 className="vertical-timeline-element--work"
-                date="2011 - present"
+                date="September 2018"
                 iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
                 // icon={<Icon/>}
               >
-                <h3 className="vertical-timeline-element-title">Creative Director</h3>
-                <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-                <p>
+                <h3 className="vertical-timeline-element-title small-title">Self-Teaching</h3>
+                <h4 className="vertical-timeline-element-subtitle small-title">React.js / Gatsby.js</h4>
+                <p className={'small-p'}>
                   Creative Direction, User Experience, Visual Design, Project Management, Team Leading
                 </p>
               </VerticalTimelineElement>
               <VerticalTimelineElement
                 className="vertical-timeline-element--work"
-                date="2010 - 2011"
-                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                date="September 2018"
+                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff'}}
                 // icon={<Icon/>}
               >
-                <h3 className="vertical-timeline-element-title">Art Director</h3>
-                <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                <h3 className="vertical-timeline-element-title">2nd Year</h3>
+                <h4 className="vertical-timeline-element-subtitle">Solent University</h4>
                 <p>
                   Creative Direction, User Experience, Visual Design, SEO, Online Marketing
                 </p>
               </VerticalTimelineElement>
               <VerticalTimelineElement
                 className="vertical-timeline-element--work"
-                date="2008 - 2010"
+                date="September 2018"
                 iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
                 // icon={<Icon/>}
               >
-                <h3 className="vertical-timeline-element-title">Web Designer</h3>
-                <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
+                <h3 className="vertical-timeline-element-title">Freelance Work</h3>
+                <h4 className="vertical-timeline-element-subtitle">SEO Optimisation</h4>
                 <p>
+                  <a href="https://northhertsjoinery.com/">https://northhertsjoinery.com/</a>
+                </p>
+              </VerticalTimelineElement>
+              <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date="June - September 2018"
+                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                // icon={<Icon/>}
+              >
+                <h3 className="vertical-timeline-element-title">Front-end Development</h3>
+                <h4 className="vertical-timeline-element-subtitle">3 SIDED CUBE</h4>
+                <p>
+                  User Experience, Visual Design
+                </p>
+              </VerticalTimelineElement>
+              <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date="May - June 2018"
+                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                // icon={<Icon/>}
+              >
+                <h3 className="vertical-timeline-element-title">Freelance Work</h3>
+                <h4 className="vertical-timeline-element-subtitle">Bespoke Website - Garden Furniture</h4>
+                <p>
+                  <a href="http://brdfurniture.co.uk/">http://brdfurniture.co.uk/</a>
+                </p>
+              </VerticalTimelineElement>
+              <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date="2006 - 2008"
+                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff'}}
+                // icon={<Icon/>}
+              >
+                <h3 className="vertical-timeline-element-title small-title">Self-Teaching</h3>
+                <h4 className="vertical-timeline-element-subtitle small-title">CSS Grid and Flexbox</h4>
+                <p className={"small-p"}>
                   User Experience, Visual Design
                 </p>
               </VerticalTimelineElement>
@@ -60,10 +94,10 @@ class ExperiencePage extends Component {
                 iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
                 // icon={<Icon/>}
               >
-                <h3 className="vertical-timeline-element-title">Web Designer</h3>
-                <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                <h3 className="vertical-timeline-element-title">Freelance Work</h3>
+                <h4 className="vertical-timeline-element-subtitle">Bespoke Website - Landscaping</h4>
                 <p>
-                  User Experience, Visual Design
+                  <a href="http://aberlandscaping.co.uk/">http://aberlandscaping.co.uk/</a>
                 </p>
               </VerticalTimelineElement>
               <VerticalTimelineElement
@@ -107,7 +141,6 @@ class ExperiencePage extends Component {
                 // icon={<Icon/>}
               />
             </VerticalTimeline>
-            </div>
         </div>
         </div>
       </Layout>
